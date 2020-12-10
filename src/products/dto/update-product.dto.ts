902b6645049c7048b4a1d719c99d9bd8eb87ba48
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-12-10 19:39:45 +0200

    add all controllers

---
+++ src/products/dto/update-product.dto.ts
@@ -0,0 +1,4 @@
+export class updateProduct {
+  readonly title: string;
+  readonly price: number;
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { updateProduct } from './dto/update-product.dto';
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getAll(): string {
+    return `getAll`;
+  }
+  @Get(':id')
+  getOne(@Param() params): string {
+    return `one ${params.id}`;
+  }
+
+  @Post()
+  create(@Body() CreateProductDto: CreateProductDto): string {
+    return `${CreateProductDto.title} price ${CreateProductDto.price}`;
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `remove ${id}`;
+  }
+  @Put(':id')
+  update(
+    @Body() updateProduct: updateProduct,
+    @Param('id') id: string,
+  ): string {
+    return `${updateProduct.title} ${id}`;
+  }
+}
